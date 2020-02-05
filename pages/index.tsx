645f1dc65365fc3620ca5927bd1399b5579984ad
kevinGwon <developerGwon@gmail.com>
2020-02-05 14:56:36 +0900

    Test dynamic route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
 import Head from 'next/head';
 import axios from 'axios';
-import Routes from '../routes';
+import Link from 'next/link';
 import '../scss/style.scss';
 
 const Index = props => {
-  const { Link } = Routes;
+  // const { Link } = Routes;
 
   return (
     <>
       <Head>
         <title>Portfolio of KevinGwon | Home</title>
       </Head>
-      <Link route="detail" params={{ id: 300 }}>
+      <Link href="/detail/[id]" as={'/detail/300'}>
         <a>상세페이지 이동</a>
       </Link>
+      {/* <Link route="detail" params={{ id: 300 }}>
+        <a>상세페이지 이동</a>
+      </Link> */}
     </>
   );
 };
