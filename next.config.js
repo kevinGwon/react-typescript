876d89024354a40dcc9945a8cb82b252f7b0da42
kevinGwon <developerGwon@gmail.com>
2020-02-05 18:27:48 +0900

    Update config - next, postcss

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
 const withSass = require('@zeit/next-sass');
+const globImporter = require('node-sass-glob-importer');
+const isDev = process.env.NODE_ENV === 'development' ? true : false;
 
 module.exports = withSass({
-  sassLoaderOptions: {
-    sourceMap: true,
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.s[ac]ss$/i,
+      use: [
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: isDev,
+            sassOptions: {
+              importer: globImporter(),
+            },
+          },
+        },
+      ],
+    });
+    return config;
   },
-  cssModules: true,
 });
---
+++ postcss.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  plugins:
+    process.env.NODE_ENV === 'production' ? [require('autoprefixer')] : [],
+};
