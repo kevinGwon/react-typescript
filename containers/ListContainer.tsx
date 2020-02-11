f56a9691723aad6ca295dc49d8a937a3a16c518b
kevinGwon <developerGwon@gmail.com>
2020-02-12 04:19:10 +0900

    Update SSR 적용

--- containers/ListContainer.tsx
+++ containers/ListContainer.tsx
@@ -1,24 +1,8 @@
 import * as React from 'react';
 import List from '../components/List';
-import { useDispatch } from 'react-redux';
-
-// API
-import { API_LIST } from '../modules/api/list';
 
 function ListContainer(data) {
-  const dispatch = useDispatch();
-
-  React.useEffect(() => {
-    if (!data.isLoading) {
-      API_LIST({ dispatch, data });
-    }
-  }, []);
-
   return <List {...data} />;
 }
-
-ListContainer.getInitialProps = async () => {
-  return {};
-};
 
 export default React.memo(ListContainer);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,49 @@
 import { NextPage } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
-import { useSelector } from 'react-redux';
-import ListContainer from '../containers/ListContainer';
+import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
+import { useEffect } from 'react';
+
+// Components
+import ListContainer from '../containers/ListContainer';
 
 // Type
 import { RootState } from '../redux/reducers';
-import { CategoryType } from '../redux/reducers/list';
+import {
+  CategoryType,
+  ListType,
+  LIST_STATE,
+  API_LIST,
+} from '../redux/reducers/list';
+import extend, { ExtendType } from '../modules/extend';
+import { API_CONFIG } from '../modules/api/api.config';
 
-interface Props {
-  data?: string[];
+interface Test {
+  API: {
+    action: ListType[];
+    thriller: ListType[];
+    crime: ListType[];
+    war: ListType[];
+    horror: ListType[];
+    romance: ListType[];
+    animation: ListType[];
+  };
 }
 
-const Index: NextPage<Props> = ({ data }) => {
+const Index: NextPage<Test> = ({ API }) => {
   const { genres }: { genres: CategoryType } = useSelector(
     (store: RootState) => store.list,
   );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    Object.keys(API).map(category => {
+      !genres[category].isLoading &&
+        API_LIST({ dispatch, category, data: API[category] });
+    });
+  }, []);
+
   return (
     <>
       <Head>
@@ -36,8 +63,104 @@ const Index: NextPage<Props> = ({ data }) => {
   );
 };
 
-Index.getInitialProps = async props => {
-  return {};
+Index.getInitialProps = async ctx => {
+  let getUrl = payload => {
+    return `https://api.themoviedb.org/3/discover/movie?api_key=${payload.key}&language=${payload.lang}&release_date.gte=${payload.year}-${payload.month}-${payload.day}&with_genres=${payload.code}&sort_by=popularity.desc&include_adult=true&include_video=true&page=1`;
+  };
+  const opt: ExtendType = extend(API_CONFIG, {
+    year: LIST_STATE.year,
+    month: LIST_STATE.month,
+    day: LIST_STATE.day,
+  });
+  const { genres } = LIST_STATE;
+  const action = await axios({
+    method: 'get',
+    url: getUrl({
+      key: opt.key,
+      lang: opt.lang,
+      year: opt.year,
+      month: opt.month,
+      day: opt.day,
+      code: genres.action.code,
+    }),
+  });
+  const thriller = await axios({
+    method: 'get',
+    url: getUrl({
+      key: opt.key,
+      lang: opt.lang,
+      year: opt.year,
+      month: opt.month,
+      day: opt.day,
+      code: genres.thriller.code,
+    }),
+  });
+  const crime = await axios({
+    method: 'get',
+    url: getUrl({
+      key: opt.key,
+      lang: opt.lang,
+      year: opt.year,
+      month: opt.month,
+      day: opt.day,
+      code: genres.crime.code,
+    }),
+  });
+  const war = await axios({
+    method: 'get',
+    url: getUrl({
+      key: opt.key,
+      lang: opt.lang,
+      year: opt.year,
+      month: opt.month,
+      day: opt.day,
+      code: genres.war.code,
+    }),
+  });
+  const horror = await axios({
+    method: 'get',
+    url: getUrl({
+      key: opt.key,
+      lang: opt.lang,
+      year: opt.year,
+      month: opt.month,
+      day: opt.day,
+      code: genres.horror.code,
+    }),
+  });
+  const romance = await axios({
+    method: 'get',
+    url: getUrl({
+      key: opt.key,
+      lang: opt.lang,
+      year: opt.year,
+      month: opt.month,
+      day: opt.day,
+      code: genres.romance.code,
+    }),
+  });
+  const animation = await axios({
+    method: 'get',
+    url: getUrl({
+      key: opt.key,
+      lang: opt.lang,
+      year: opt.year,
+      month: opt.month,
+      day: opt.day,
+      code: genres.animation.code,
+    }),
+  });
+  return {
+    API: {
+      action: action.data.results,
+      thriller: thriller.data.results,
+      crime: crime.data.results,
+      war: war.data.results,
+      horror: horror.data.results,
+      romance: romance.data.results,
+      animation: animation.data.results,
+    },
+  };
 };
 
 export default Index;
