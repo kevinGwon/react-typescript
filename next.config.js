4800fd684e09e9fadd10aa682b13781f2fb83376
kevinGwon <developerGwon@gmail.com>
2020-02-12 13:23:28 +0900

    Fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
+require('dotenv').config();
 const withSass = require('@zeit/next-sass');
 const globImporter = require('node-sass-glob-importer');
 const isDev = process.env.NODE_ENV === 'development' ? true : false;
 
 module.exports = withSass({
-  webpack: config => {
+  webpack: (config, { webpack }) => {
+    config.node = {
+      fs: 'empty',
+    };
+
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
+      return acc;
+    }, {});
+
+    config.plugins.push(new webpack.DefinePlugin(env));
+
     config.module.rules.push({
       test: /\.s[ac]ss$/i,
       use: [
