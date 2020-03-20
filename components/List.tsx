3712f5a42aa1786a3cdd34fdf185fc30adffa7de
kevinGwon <developerGwon@gmail.com>
2020-03-20 16:25:17 +0900

    Fix List -> ListContainer로 분리

--- components/List.tsx
+++ components/List.tsx
@@ -1,144 +1,90 @@
-import * as React from 'react';
-import { TweenMax } from 'gsap';
-import Swiper from 'swiper';
+import React, { useRef, useCallback, useEffect } from 'react';
 import Link from 'next/link';
+import Swiper from 'swiper';
 import filterImages from '../modules/filterImages';
-import { CategoryType } from '../types/redux/list';
+import { ListType } from '../types/redux/list';
 
-function List(data: CategoryType) {
-  const $sectionBg = React.useRef();
-
-  const runBackUpBg = React.useCallback(obj => {
-    const $div = document.createElement('div');
-    obj.slideLength === 1
-      ? $div.classList.add('movie-section-bg', 'movie-section-bg--next')
-      : $div.classList.add('movie-section-bg');
-    $div.style.backgroundImage = `url('${obj.bgUrl}')`;
-
-    if (obj.slideLength === 1) {
-      obj.$bg.after($div);
-    } else {
-      console.log('------ Slide overFlow || Reset Slide ------');
-      obj.$section.prepend($div);
-    }
-  }, []);
-
-  const runTransition = React.useCallback(
-    $target => {
-      const index = $target.snapIndex,
-        $slide = $target.slides[index],
-        $section = $slide.parentNode.offsetParent.offsetParent.offsetParent,
-        $bgAll = $section.querySelectorAll('.movie-section-bg'),
-        $bg = $section.querySelector('.movie-section-bg'),
-        bgUrl = $slide.querySelector('img').getAttribute('data-bg'),
-        slideLength = $bgAll.length;
-
-      if ($bgAll.length > 1) {
-        $bgAll.forEach(item => {
-          item.remove();
-        });
-        runBackUpBg({ slideLength, $section, bgUrl });
-        return false;
-      }
-
-      let $prev = null,
-        $current = $bg,
-        $next = null;
-
-      runBackUpBg({ slideLength, $bg, bgUrl });
-
-      $next = $section.querySelector('.movie-section-bg--next');
-      $current = $next;
-      $prev = $current.previousElementSibling;
-
-      TweenMax.to($next, 1 / 1.5, {
-        scale: 1,
-      });
-
-      TweenMax.to($prev, 1, {
-        autoAlpha: 0,
-        rotation: -25,
-        onComplete: () => {
-          $current.classList.remove('movie-section-bg--next');
-          $prev.remove();
+function List({
+  data,
+  $sectionBg,
+  runTransition,
+}: {
+  data: ListType;
+  $sectionBg: () => void;
+  runTransition: any;
+}) {
+  useEffect(() => {
+    let swiper = null;
+    if (data[0].category !== 'search') {
+      swiper = new Swiper(`.swiper-container-${data[0].category}`, {
+        lazy: true,
+        parallax: true,
+        centeredSlides: true,
+        slidesPerView: 3,
+        spaceBetween: 5,
+        grabCursor: true,
+        speed: 800,
+        effect: 'coverflow',
+        coverflowEffect: {
+          rotate: 50, // Slide rotate in degrees
+          stretch: 0, // Stretch space between slides (in px)
+          depth: 100, // Depth offset in px (slides translate in Z axis)
+          modifier: 1, // Effect multipler
+          slideShadows: true, // Enables slides shadows
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          dynamicBullets: true,
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        on: {
+          slideChange() {
+            runTransition(this);
+          },
         },
       });
-    },
-    [runBackUpBg],
-  );
-
-  React.useEffect(() => {
-    let swiper = null;
-    if (data.category !== 'search') {
-      setTimeout(() => {
-        swiper = new Swiper(`.swiper-container-${data.category}`, {
-          lazy: true,
-          parallax: true,
-          centeredSlides: true,
-          slidesPerView: 3,
-          spaceBetween: 5,
-          grabCursor: true,
-          speed: 800,
-          effect: 'coverflow',
-          coverflowEffect: {
-            rotate: 50, // Slide rotate in degrees
-            stretch: 0, // Stretch space between slides (in px)
-            depth: 100, // Depth offset in px (slides translate in Z axis)
-            modifier: 1, // Effect multipler
-            slideShadows: true, // Enables slides shadows
-          },
-          pagination: {
-            el: '.swiper-pagination',
-            dynamicBullets: true,
-            clickable: true,
-          },
-          navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          },
-          on: {
-            slideChange() {
-              runTransition(this);
-            },
-          },
-        });
-      }, 500);
     }
     return () => {
       swiper.destroy();
       swiper = null;
     };
-  }, [data.category, runTransition]);
+  }, [runTransition]);
 
   return (
     <section
       className={`movie-section${
-        data.category === 'action' ? ' is-active' : ''
+        data[0].category === 'action' ? ' is-active' : ''
       }`}
     >
       <div
         className="movie-section-bg"
         style={
-          data.list[0] && {
-            backgroundImage: `url('${data.list[0].bgImage}')`,
+          data[0] && {
+            backgroundImage: `url('${data[0].bgImage}')`,
           }
         }
         ref={$sectionBg}
       ></div>
       <div className="l-wrap">
         <h2 className="h1">
-          {data.category === 'action' && '액션 '}
-          {data.category === 'thriller' && '스릴러 '}
-          {data.category === 'crime' && '범죄 '}
-          {data.category === 'war' && '전쟁 '}
-          {data.category === 'horror' && '공포 '}
-          {data.category === 'romance' && '로맨스 '}
-          {data.category === 'animation' && '애니메이션 '}
+          {data[0].category === 'action' && '액션 '}
+          {data[0].category === 'thriller' && '스릴러 '}
+          {data[0].category === 'crime' && '범죄 '}
+          {data[0].category === 'war' && '전쟁 '}
+          {data[0].category === 'horror' && '공포 '}
+          {data[0].category === 'romance' && '로맨스 '}
+          {data[0].category === 'animation' && '애니메이션 '}
           영화
         </h2>
-        <div className={`swiper-container swiper-container-${data.category}`}>
+        <div
+          className={`swiper-container swiper-container-${data[0].category}`}
+        >
           <div className="swiper-wrapper">
-            {data.list.map(item => (
+            {data.map(item => (
               <div key={item.id} className="swiper-slide">
                 <Link href="/detail/[id]" as={`/detail/${item.id}`}>
                   <a>
@@ -155,7 +101,7 @@ function List(data: CategoryType) {
                             : 'http://placehold.it/3840x2160?text=Not Found'
                         }
                         className="swiper-lazy"
-                        alt=""
+                        alt={item.title}
                       />
                       <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
                     </div>
---
+++ containers/ListContainer.tsx
@@ -0,0 +1,72 @@
+import React, { useRef, useCallback, useEffect } from 'react';
+import { TweenMax } from 'gsap';
+import Link from 'next/link';
+import List from '../components/List';
+
+function ListContainer(props) {
+  const $sectionBg = useRef();
+
+  const runBackUpBg = useCallback(obj => {
+    const $div = document.createElement('div');
+    obj.slideLength === 1
+      ? $div.classList.add('movie-section-bg', 'movie-section-bg--next')
+      : $div.classList.add('movie-section-bg');
+    $div.style.backgroundImage = `url('${obj.bgUrl}')`;
+
+    if (obj.slideLength === 1) {
+      obj.$bg.after($div);
+    } else {
+      console.log('------ Slide overFlow || Reset Slide ------');
+      obj.$section.prepend($div);
+    }
+  }, []);
+
+  const runTransition = useCallback(
+    $target => {
+      const index = $target.snapIndex,
+        $slide = $target.slides[index],
+        $section = $slide.parentNode.offsetParent.offsetParent.offsetParent,
+        $bgAll = $section.querySelectorAll('.movie-section-bg'),
+        $bg = $section.querySelector('.movie-section-bg'),
+        bgUrl = $slide.querySelector('img').getAttribute('data-bg'),
+        slideLength = $bgAll.length;
+
+      if ($bgAll.length > 1) {
+        $bgAll.forEach(item => {
+          item.remove();
+        });
+        runBackUpBg({ slideLength, $section, bgUrl });
+        return false;
+      }
+
+      let $prev = null,
+        $current = $bg,
+        $next = null;
+
+      runBackUpBg({ slideLength, $bg, bgUrl });
+
+      $next = $section.querySelector('.movie-section-bg--next');
+      $current = $next;
+      $prev = $current.previousElementSibling;
+
+      TweenMax.to($next, 1 / 1.5, {
+        scale: 1,
+      });
+
+      TweenMax.to($prev, 1, {
+        autoAlpha: 0,
+        rotation: -25,
+        onComplete: () => {
+          $current.classList.remove('movie-section-bg--next');
+          $prev.remove();
+        },
+      });
+    },
+    [runBackUpBg],
+  );
+  return (
+    <List {...props} $sectionBg={$sectionBg} runTransition={runTransition} />
+  );
+}
+
+export default ListContainer;
