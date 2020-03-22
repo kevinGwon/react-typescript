0ed18186ba610f314d3bcda2bae54a82c022b086
kevinGwon <developerGwon@gmail.com>
2020-03-23 01:24:50 +0900

    Fix MainListContainer - useCallback으로 swiper함수 선언

--- containers/main/MainListContainer.tsx
+++ containers/main/MainListContainer.tsx
@@ -1,10 +1,11 @@
 import React, { useRef, useCallback, useEffect } from 'react';
 import MainList from '../../components/main/MainList';
 import { ListType } from '../../types/redux/list';
-import runSwiper from '../../modules/swiper';
+import swiper from '../../modules/swiper';
 
 function MainListContainer(props: { data: ListType[] }) {
   const $sectionBg = useRef();
+  const runSwiper = useCallback(swiper, []);
   return <MainList {...props} $sectionBg={$sectionBg} runSwiper={runSwiper} />;
 }
 
