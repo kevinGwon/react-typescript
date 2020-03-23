deacdd563ea75f2052ac6fd20c8622d6d4b85d0b
kevinGwon <developerGwon@gmail.com>
2020-03-23 18:25:08 +0900

    Fix Firebase 관련코드 제거

--- containers/HeaderContainer.tsx
+++ containers/HeaderContainer.tsx
@@ -1,37 +1,13 @@
 import React, { useCallback, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import Router from 'next/router';
 import Header from '../components/Header';
-import { USER_INFO } from '../redux/reducers/user';
-import firebase from '../firebase';
-import 'firebase/auth';
 
 function HeaderContainer() {
-  const user = useSelector(store => store.user);
-  const dispatch = useDispatch();
   const [searchActive, setSearchActive] = useState(false);
-  const onLogin = useCallback(
-    e => {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      firebase
-        .auth()
-        .signInWithPopup(provider)
-        .then(res => {
-          dispatch({
-            type: USER_INFO,
-            name: res.user.displayName,
-            email: res.user.email,
-            uid: res.user.uid,
-          });
-        })
-        .catch(error => {
-          alert(error);
-        });
-    },
-    [firebase],
-  );
   const runSubmit = useCallback((e: React.FormEvent) => {
     e.preventDefault();
     e.stopPropagation();
+    Router.push('/search');
   }, []);
   const runSearchActive = useCallback(() => {
     setSearchActive(!searchActive);
