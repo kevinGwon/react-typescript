9741971843c35102a2bd88530df2e28d684ce2f2
kevinGwon <developerGwon@gmail.com>
2020-03-24 16:00:32 +0900

    Fix ThemoveDB session 받아오는 코드 임시 추가

--- containers/HeaderContainer.tsx
+++ containers/HeaderContainer.tsx
@@ -1,6 +1,8 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import Router from 'next/router';
 import Header from '../components/Header';
+import { LOGIN } from '../modules/api/login';
+import Axios from 'axios';
 
 function HeaderContainer() {
   const [searchActive, setSearchActive] = useState(false);
@@ -12,6 +14,31 @@ function HeaderContainer() {
   const runSearchActive = useCallback(() => {
     setSearchActive(!searchActive);
   }, [searchActive]);
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const session = localStorage.getItem('session');
+    if (!token) {
+      const login = async () => {
+        try {
+          await LOGIN();
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      login();
+    }
+    const test = async () => {
+      try {
+        const res = await Axios.get(
+          `https://api.themoviedb.org/3/account?api_key=1e006c1e39b26bfadaa6f757bc1435cf&session_id=${session}`,
+        );
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    test();
+  }, []);
   return (
     <Header
       searchActive={searchActive}
