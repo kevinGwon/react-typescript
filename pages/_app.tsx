e1a00d179dba93762a5b1c3ea8dae04c6d482406
kevinGwon <developerGwon@gmail.com>
2020-03-26 00:55:42 +0900

    Fix StyledForm - 필요한 스타일만 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ import LoginContainer from '../containers/LoginContainer';
 // Styled
 import StyledReset from '../styled/global/StyledReset.style';
 import StyledHelper from '../styled/global/StyledHelper.style';
+import StyledForm from '../styled/global/StyledForm.style';
 import Loading from '../components/common/Loading';
 
 const App = ({ Component, pageProps, store }) => {
@@ -26,6 +27,7 @@ const App = ({ Component, pageProps, store }) => {
         <Footer />
         <LoginContainer />
         <Loading />
+        <StyledForm />
         <StyledReset />
         <StyledHelper />
         {/* PersistGate는 persisted된 상태가 Redux에 저장될때 까지 렌더링을 지연시킨다. 따라서 SSR에 사용할 경우 영향을 준다.. */}
--- styled/global/StyledForm.style.tsx
+++ styled/global/StyledForm.style.tsx
@@ -16,74 +16,6 @@ export default createGlobalStyle`
   // Form
   // -----------------------------------------------------------------------------
 
-  [type=text],
-  [type=number],
-  [type=tel],
-  [type=search],
-  [type=password],
-  [type=email],
-  [type=url],
-  [type=date],
-  [type=datetime-local],
-  [type=file],
-  select,
-  textarea {
-    box-sizing: border-box;
-    padding: ${$inputPaddingTb} ${$inputPaddingLr};
-    border: 1px solid ${rgba($colorPrimary, 0.8)};
-    background: transparent;
-    color: ${$white};
-    font-size: 16px;
-    line-height: 1.25;
-    outline: none;
-    vertical-align: middle;
-
-    -webkit-border-radius: 0;
-    -webkit-appearance: none;
-    transition: border-color 0.3s;
-
-
-    &[readonly] {
-
-      &:hover,
-      &:focus {
-        border-color: ${$silver};
-      }
-    }
-
-    &[disabled] {
-      cursor: not-allowed;
-      color: ${$silverDark};
-      background: ${$fog};
-
-      &:hover {
-        border-color: ${$silver};
-      }
-    }
-  }
-
-  input {
-    height: 46px;
-  }
-
-  select {
-    display: inline-block;
-    padding-right: 40px;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-
-    &::-ms-expand {
-      display: none;
-    }
-  }
-
-  textarea {
-    max-width: 100%;
-    min-height: 100px;
-    line-height: 1.6;
-  }
-
-  // placeholder 색상
   ::-webkit-input-placeholder {
     color: ${$white};
   }
@@ -127,46 +59,6 @@ export default createGlobalStyle`
     &::-webkit-search-results-button,
     &::-webkit-search-results-decoration {
       display: none;
-    }
-  }
-
-  // date type 컨트롤에서 관련 버튼 감춤
-  input[type=date],
-  input[type=datetime-local] {
-    &::-webkit-inner-spin-button {
-      display: none;
-      margin: 0;
-    }
-
-    &::-webkit-clear-button,
-    &::-webkit-calendar-picker-indicator {
-      display: none;
-    }
-
-    &::-webkit-datetime-edit-fields-wrapper {
-      padding: 0;
-    }
-  }
-
-  // input size
-  .input {
-    &-block {
-      display: block;
-      width: 100%;
-    }
-
-    &--sm {
-      padding: ${$inputPaddingTb} - 2px ${$inputPaddingLr};
-      font-size: 14px;
-      line-height: 14px;
-      height: 36px;
-    }
-
-    &--lg {
-      padding: ${$inputPaddingTb} + 2px ${$inputPaddingLr};
-      font-size: 18px;
-      line-height: 34px;
-      height: 56px;
     }
   }
 `;
