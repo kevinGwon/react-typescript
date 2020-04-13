31950583cc966a2f46878dfa3747e79139fbedf5
kevinGwon <developerGwon@gmail.com>
2020-04-13 16:16:00 +0900

    Fix React 선언문 수정

--- components/common/Article.tsx
+++ components/common/Article.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 // Components
 import HeaderContainer from '../../containers/HeaderContainer';
--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { StyledFooter } from './Footer.style';
 
 function Footer() {
