a183263710ec688c9230cb35eeea4e32383a7aef
kevinGwon <developerGwon@gmail.com>
2020-04-16 01:45:04 +0900

    Fix HeaderSearch - Connect hoc로 변경

--- components/common/header/HeaderSearch.tsx
+++ components/common/header/HeaderSearch.tsx
@@ -17,36 +17,51 @@ import { StyledSearchUI } from '../SearchUI.style';
 import SearchUI from '../SearchUI';
 
 function HeaderSearch({
+  pageType,
   search,
   searchActive,
+  setSearchActive,
   runSubmit,
   runSearchActive,
   runCloseSearch,
   runCloseBlock,
 }: {
+  pageType: null | string;
   search: string;
   searchActive: boolean;
-  runSubmit: (e: React.FormEvent) => void;
-  runSearchActive: () => void;
-  runCloseSearch: () => void;
-  runCloseBlock: (e: React.MouseEvent) => void;
+  setSearchActive: React.Dispatch<React.SetStateAction<boolean>>;
+  runSubmit: (
+    e: React.FormEvent,
+    setSearchActive: React.Dispatch<React.SetStateAction<boolean>>,
+  ) => any;
+  runSearchActive: (
+    setSearchActive: React.Dispatch<React.SetStateAction<boolean>>,
+    searchActive: boolean,
+  ) => any;
+  runCloseSearch: (
+    setSearchActive: React.Dispatch<React.SetStateAction<boolean>>,
+  ) => any;
+  runCloseBlock: (e: React.MouseEvent) => any;
 }) {
   return (
     <StyledHeaderSearch>
-      <StyledHeaderForm onSubmit={e => runSubmit(e)}>
+      <StyledHeaderForm onSubmit={e => runSubmit(e, setSearchActive)}>
         <StyledHeaderSearchInputGroup>
           {searchActive && (
-            <DimLayer callback={runCloseSearch}>
+            <DimLayer callback={() => runCloseSearch(setSearchActive)}>
               <label htmlFor="search" className="a11y">
                 search
               </label>
-              <div className="DimLayer-inner" onClick={runCloseBlock}>
+              <div className="DimLayer-inner" onClick={e => runCloseBlock(e)}>
                 <SearchUI id="search" placeholder="검색어를 입력하세요" />
               </div>
             </DimLayer>
           )}
           {!search.length && (
-            <StyledHeaderSearchBtn type="button" onClick={runSearchActive}>
+            <StyledHeaderSearchBtn
+              type="button"
+              onClick={() => runSearchActive(setSearchActive, searchActive)}
+            >
               <IconSearch />
               <span className="a11y">검색열기</span>
             </StyledHeaderSearchBtn>
