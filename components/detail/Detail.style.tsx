b55a86c23ae163b019688e4c26367a5ff4334fa7
kevinGwon <developerGwon@gmail.com>
2020-04-16 17:50:24 +0900

    Fix Detail - 등록된 정보가 없을때 홈으로 이동

--- components/detail/Detail.style.tsx
+++ components/detail/Detail.style.tsx
@@ -214,3 +214,16 @@ export const StyledDetailFavoriteBtn = styled.button`
   height: 30px;
   fill: ${$colorPrimary};
 `;
+
+export const StyledDetailError = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 91vh;
+
+  p {
+    display: block;
+    font-size: 1.6rem;
+  }
+`;
