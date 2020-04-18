a26e17fe81f4b2e192e8c0d58cbbe5afa060c822
kevinGwon <developerGwon@gmail.com>
2020-04-18 14:46:53 +0900

    Fix StyledGlobal 파일명 수정

--- styled/global/StyledGlobal.tsx
+++ styled/global/StyledGlobal.tsx
@@ -2,11 +2,11 @@ import { createGlobalStyle } from 'styled-components';
 import StyledReset from './StyledReset.style';
 import StyledHelper from './StyledHelper.style';
 import StyledForm from './StyledForm.style';
-import StyledStateStyle from './StyledState.style';
+import StyledCommon from './StyledCommon.style';
 
 export default createGlobalStyle`
   ${StyledReset};
   ${StyledHelper};
   ${StyledForm};
-  ${StyledStateStyle};
+  ${StyledCommon};
 `;
