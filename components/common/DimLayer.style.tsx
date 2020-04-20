07fd0d118c81224262e1fee4124c84db67a0c32e
kevinGwon <developerGwon@gmail.com>
2020-04-20 15:21:29 +0900

    Fix Header - DimLayer 여백 조정

--- components/common/DimLayer.style.tsx
+++ components/common/DimLayer.style.tsx
@@ -5,14 +5,18 @@ import { $black } from '../../styled/global/StyledVariable.style';
 import { rgba, FullPos } from '../../styled/mixin';
 
 export const StyledDimLayer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: fixed;
   left: 0;
   top: 0;
   right: 0;
   bottom: 0;
   z-index: ${props => props.z || 100};
-  display: flex;
-  justify-content: center;
-  align-items: center;
   background-color: ${rgba($black, 0.9)};
+
+  .DimLayer-inner {
+    padding: 0 2rem;
+  }
 `;
